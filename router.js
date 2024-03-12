d336e8a8a6f70f56f27a61bb96e138711ff1c661
FredNurk217 <josea.solis@cetys.edu.mx>
2024-03-12 12:40:41 -0700

    add route modelo/sabor

--- router.js
+++ router.js
@@ -30,6 +30,24 @@ router.get('/sabores', (req, res) => {
     });
 });
 
+router.get('/modelos/sabor/:id', (req, res) => {
+    const client = new Client(connectionData);
+    client.connect()
+    .then(() => {
+        return client.query('select s.sabor from vape v inner join modelo m on v.fkid_modelo = m.id_modelo inner join sabor s on v.fkid_sabor = s.id_sabor where m.id_modelo = $1;', [req.params.id]);
+    })
+    .then(result => {
+        res.status(200).send(result.rows);
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(400).send('Error');
+    })
+    .finally(() => {
+        client.end();
+    });
+})
+
 //POST sabor
 router.post('/sabor', (req, res) => {
     const client = new Client(connectionData);
@@ -73,7 +91,7 @@ router.get('/vapes', (req, res) => {
     const client = new Client(connectionData);
     client.connect()
     .then(() => {
-        return client.query('select v.*, s.sabor, m.modelo, mc.marca from vape v inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca;');
+        return client.query('select v.*, s.sabor, m.modelo, mc.marca, m.precio from vape v inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca;');
     })
     .then(result => {
         res.status(200).send(result.rows);
@@ -92,7 +110,7 @@ router.get('/carrito/vape', (req, res) => {
     const client = new Client(connectionData);
     client.connect()
     .then(() => {
-        return client.query('select c.*, cv.*, v.*, s.sabor, m.modelo, mc.marca from carrito_vape cv inner join carrito c on c.id_carrito = cv.fkid_carrito inner join vape v on v.id_vape = cv.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca order by id_carrito;');
+        return client.query('select c.*, cv.*, v.*, s.sabor, m.modelo, m.precio, mc.marca from carrito_vape cv inner join carrito c on c.id_carrito = cv.fkid_carrito inner join vape v on v.id_vape = cv.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca order by id_carrito;');
     })
     .then(result => {
         res.status(200).send(result.rows);
@@ -112,7 +130,7 @@ router.get('/carrito/vape/:id', (req, res) => {
     client.connect()
     .then(() => {
         const idCarrito = req.params.id;
-        return client.query('select c.*, cv.*, v.*, s.sabor, m.modelo, mc.marca from carrito_vape cv inner join carrito c on c.id_carrito = cv.fkid_carrito inner join vape v on v.id_vape = cv.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca where c.id_carrito = $1;', [idCarrito]);
+        return client.query('select c.*, cv.*, v.*, s.sabor, m.modelo, m.precio, mc.marca from carrito_vape cv inner join carrito c on c.id_carrito = cv.fkid_carrito inner join vape v on v.id_vape = cv.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca where c.id_carrito = $1;', [idCarrito]);
     })
     .then(result => {
         res.status(200).send(result.rows);
@@ -151,7 +169,7 @@ router.get('/orden/vape', (req, res) => {
     const client = new Client(connectionData);
     client.connect()
     .then(() => {
-        return client.query('select o.*, ov.*, v.*, s.sabor, m.modelo, mc.marca from orden_vape ov inner join orden o on o.id_orden = ov.fkid_orden inner join vape v on v.id_vape = ov.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca order by id_orden;');
+        return client.query('select o.*, ov.*, v.*, s.sabor, m.modelo, m.precio, mc.marca from orden_vape ov inner join orden o on o.id_orden = ov.fkid_orden inner join vape v on v.id_vape = ov.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca order by id_orden;');
     })
     .then(result => {
         res.status(200).send(result.rows);
@@ -171,7 +189,7 @@ router.get('/orden/vape/:id', (req, res) => {
     client.connect()
     .then(() => {
         const idOrden = req.params.id;
-        return client.query('select o.*, ov.*, v.*, s.sabor, m.modelo, mc.marca from orden_vape ov inner join orden o on o.id_orden = ov.fkid_orden inner join vape v on v.id_vape = ov.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca where o.id_orden = $1;', [idOrden]);
+        return client.query('select o.*, ov.*, v.*, s.sabor, m.modelo, m.precio, mc.marca from orden_vape ov inner join orden o on o.id_orden = ov.fkid_orden inner join vape v on v.id_vape = ov.fkid_vape inner join sabor s on v.fkid_sabor = s.id_sabor inner join modelo m on v.fkid_modelo = m.id_modelo inner join marca mc on m.fkid_marca = mc.id_marca where o.id_orden = $1;', [idOrden]);
     })
     .then(result => {
         res.status(200).send(result.rows);
